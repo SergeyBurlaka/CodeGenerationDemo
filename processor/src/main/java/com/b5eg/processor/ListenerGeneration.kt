22848edba6f2ebb0d9cbff8d2f224a2ef4edc8c5
SergeyBurlaka <burlakasergeydev@gmail.com>
2019-02-03 18:13:46 +0200

    feature: added generate for methods

--- processor/src/main/java/com/b5eg/processor/ListenerGeneration.kt
+++ processor/src/main/java/com/b5eg/processor/ListenerGeneration.kt
@@ -1,5 +1,6 @@
 package com.b5eg.processor
 
+import com.b5eg.annotations.BindAction
 import com.b5eg.annotations.BindField
 import com.b5eg.annotations.BindListener
 import com.google.auto.service.AutoService
@@ -8,8 +9,8 @@ import java.io.File
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.TypeElement
-import javax.lang.model.element.VariableElement
 import javax.lang.model.util.ElementFilter
 
 @AutoService(Processor::class) // For registering the service
@@ -23,8 +24,6 @@ class ListenerGeneration : AbstractProcessor() {
 
     override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
 
-        // val bricks = HashMap<VariableElement, String/*ArrayList<String>*/>()
-
         val generatedSourcesRoot: String =
             processingEnv.options[BindFieldsProcessor.KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
 
@@ -33,64 +32,53 @@ class ListenerGeneration : AbstractProcessor() {
             return false
         }
 
-        val variables = HashSet<TypeElement>()
+        val methods = ArrayList<ExecutableElement>()
+        roundEnv.getElementsAnnotatedWith(BindAction::class.java).forEach { methodElement ->
+            if (methodElement.kind != ElementKind.METHOD) {
+                processingEnv.messager.errormessage { "Can only be applied to functions,  element: $methodElement " }
+                return false
+            }
+            (methodElement as ExecutableElement).apply {
+                methods.add(this)
+            }
+        }
+
+        val listeners = HashSet<TypeElement>()
 
         roundEnv.getElementsAnnotatedWith(BindListener::class.java).forEach { methodElement ->
-
             if (methodElement.kind != ElementKind.INTERFACE) {
                 processingEnv.messager.errormessage { "Can only be applied to functions,  element: $methodElement " }
                 return false
             }
-
             (methodElement as TypeElement).apply {
-
-                variables.add(this)
-
-//                parameters[0].also { variableElement ->
-//
-//
-//                    //поле при аргументе метода
-//                    //  val fieldsInArgument = ElementFilter.fieldsIn(variableAsElement.enclosedElements)
-//
-//                    //from anotation
-//                    //  val methodName = methodElement.getAnnotation(BindListener::class.java).methodName
-//
-//                    bricks.put(variableElement, this.simpleName.toString())
-//
-//                    variables.add(variableElement)
-//
-//                }
+                listeners.add(this)
             }
         }
 
-        //аргументы при анотации
-        //val annotationArgs = method.getAnnotation(BindField::class.java).viewIds
+        listeners.forEach { variableElement ->
 
-
-        variables.forEach { variableElement ->
-
+            //BUILD METHOD HEAD
             val funcBuilder =
-
                 FunSpec.builder("bindFields2")
-
                     .addModifiers(KModifier.PUBLIC)
                     .addParameter(
                         name = "listener",
                         type = variableElement.asType().asTypeName()
                     )
 
-            val variableAsElement1 = processingEnv.typeUtils.asElement(variableElement.asType())
+            //BUILD METHOD BODY
 
+            val variableAsElement1 = processingEnv.typeUtils.asElement(variableElement.asType())
             funcBuilder.addStatement(
                 // "listener.%L()"+"\n"+
-                "%T.d(\"HACK\",\"try ${variableAsElement1.enclosedElements.size}\")",
+                "%T.d(\"HACK\",\" ${variableAsElement1.enclosedElements.size}\")",
                 // str,
                 ClassName("android.util", "Log")
             )
 
             funcBuilder.addStatement(
                 // "listener.%L()"+"\n"+
-                "%T.d(\"HACK\",\"try ${variableAsElement1.asType().asTypeName()}\")",
+                "%T.d(\"HACK\",\" ${variableAsElement1.asType().asTypeName()}\")",
                 // str,
                 ClassName("android.util", "Log")
             )
@@ -104,10 +92,21 @@ class ListenerGeneration : AbstractProcessor() {
                 )
             }
 
+            /**
+             * methods
+             */
+            methods.forEach {
+                funcBuilder.addStatement(
+                    // "listener.%L()"+"\n"+
+                    "%T.d(\"HACK\",\" ${it.simpleName}\")",
+                    // str,
+                    ClassName("android.util", "Log")
+                )
+            }
+
             val file = File(generatedSourcesRoot)
 
             file.mkdir()
-
             FileSpec.builder("com.b5eg.codegeneration", "BindFieldsGenerated2")
                 .addFunction(funcBuilder.build()).build()
                 .writeTo(file)
